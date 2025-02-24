e02b65dfdbddcfb6eea136bcb51850d98d74cb83
Neil Moffatt <neil.moffatt@outlook.com>
2025-02-24 15:15:03 -0800

    Updated

--- hello world/main.go
+++ hello world/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println("`.-::::::-.`")
+	fmt.Println(".:-::::::::::::::-:.")
+	fmt.Println("`_:::    ::    :::_`")
+	var line4 string = ".:( ^   :: ^   ):."
+	fmt.Println(line4)
+	integer := 23.1
+	fmt.Printf("%v\n", integer)
+	fmt.Printf("%v %d %x %o %b\n", integer, integer, integer, integer, integer)
+	var flavorScale float32 = 5.8
+	fmt.Println(flavorScale)
+
+	fmt.Println(math.Round(x*100) / 100)
+	amount, unit := 10, "doll hairs"
+
+	fmt.Println(amount, unit, ", that's expensive...")
 }
